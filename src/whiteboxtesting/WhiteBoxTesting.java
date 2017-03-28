25d45794f0e5fac13546fbb9351249effdeea10d
Thieu Mao <nguyen.van.thieub@framgia.com>
2017-03-28 21:04:07 +0700

    Learn Basic

--- src/whiteboxtesting/WhiteBoxTesting.java
+++ src/whiteboxtesting/WhiteBoxTesting.java
@@ -6,4 +6,19 @@ public class WhiteBoxTesting {
         System.out.println("Init Project learn White Box Testing");
     }
     
+    float foo(int a, int b, int c, int d) {
+        float e;
+        if (a==0)
+            return 0;
+        int x = 0;
+        if (((a==b) || (c==d)) && bug(a))
+            x = 1;
+        e = 1/x;
+        return 0;
+    }
+
+    private boolean bug(int a) {
+        return true;
+    }
+    
 }
